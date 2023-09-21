d3668083469365db9dfaa8b3358e8f98f41967b4
Juan Pablo Bill <juampibill1995@gmail.com>
2023-09-21 13:24:47 -0300

    second commit

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,9 +1,21 @@
 import L from "leaflet";
 import icon from "../assets/images/icon-location.svg";
 
+let iconAnchor, popupAnchor;
+
+if (window.innerWidth <= 768) {
+  // Mobile
+  iconAnchor = [16, -80];
+  popupAnchor = [2, -40];
+} else {
+  // Desktop
+  iconAnchor = [16, 41];
+  popupAnchor = [2, -20];
+}
+
 export default L.icon({
   iconSize: [32, 40],
-  iconAnchor: [10, 41],
-  popupAnchor: [2, -40],
+  iconAnchor: iconAnchor,
+  popupAnchor: popupAnchor,
   iconUrl: icon,
 });
